c147ce2dde5876cecd122572a0ba50cff057a388
NthnTrzks <115201161+NthnTrzks@users.noreply.github.com>
2022-11-13 09:20:06 -0300

    v 0.0.1 (Ajuste do Loop)

--- src/Main.java
+++ src/Main.java
@@ -102,50 +102,46 @@ public class Main{
         Scanner sc = new Scanner (System.in);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd. MMM. yyyy");
 
-        while(i == 1){
+        System.out.println("Informe o nome da Enfermeira\n");
+        String name = sc.nextLine();
+        System.out.println("Informe o cpf da Enfemeira\n");
+        String cpf = sc.nextLine();
+        Enfermeira aux = new Enfermeira(name,cpf);
+        listEnf.add(aux);
 
-            System.out.println("Informe o nome da Enfermeira\n");
-            String name = sc.nextLine();
-            System.out.println("Informe o cpf da Enfemeira\n");
-            String cpf = sc.nextLine();
-            Enfermeira aux = new Enfermeira(name,cpf);
-            listEnf.add(aux);
+        while(i == 1){
 
             System.out.println("[1] - Vacinar Cidadão\n[2] - Listar Cidadões\n[Qualquer Outra Tecla para sair]");
             opt = sc.nextInt();
 
             switch (opt){
                 case 1:
-                    /*
-                                    
-                            //Inputs e formatações - Obs: resolver situação do scanner skipping
-                            System.out.println("Informe o cpf do Cidadão:\n");
-                            String cpfC = sc.nextLine();
-                            System.out.println("Informe o nome do Cidadão\n");
-                            String nameC = sc.nextLine();
-                            System.out.println("Informe a data da Primeira Dose -\n \bInformar em dd-mm-yyyy\b \n");
-                            String date1 = sc.nextLine();
-                            LocalDate date1f = LocalDate.parse(date1,dtf);
-                            System.out.println("Informe a data da Segunda Dose -\n \bInformar em dd-mm-yyyy\b \n");
-                            String date2 = sc.nextLine();
-                            LocalDate date2f = LocalDate.parse(date2,dtf);
-                            LocalDate proxVacina = new LocalDate(date1f.getYear(), date1f.getMonthValue()+4, date1f.getDayOfMonth());
-
-                            if (proxVacina.compareTo(date2f) < 0){
-                                System.out.println("Não pode dar 2 dose");
-                            }else{
-                                System.out.println("Informe a data da Terceira Dose -\n \bInformar em dd-mm-yyyy\b \n");
-                                String date3 = sc.nextLine();
-                                LocalDate date3f = LocalDate.parse(date2,dtf);
-                                LocalDate proxVacina2 = new LocalDate(date2f.getYear(), date2f.getMonthValue()+4, date2f.getDayOfMonth());
-                                if (proxVacina2.compareTo(date3f) < 0){
-                                    System.out.println("Não pode dar 3 dose");
-                                }else{
+                    System.out.println("entrouCase");
+                    //Inputs e formatações - Obs: resolver situação do scanner skipping
+                    System.out.println("Informe o cpf do Cidadão:\n");
+                    String cpfC = sc.nextLine();
+                    System.out.println("Informe o nome do Cidadão\n");
+                    String nameC = sc.nextLine();
+                    System.out.println("Informe a data da Primeira Dose -\n \bInformar em dd-mm-yyyy\b \n");
+                    String date1 = sc.nextLine();
+                    LocalDate date1f = LocalDate.parse(date1,dtf);
+                    System.out.println("Informe a data da Segunda Dose -\n \bInformar em dd-mm-yyyy\b \n");
+                    String date2 = sc.nextLine();
+                    LocalDate date2f = LocalDate.parse(date2,dtf);
+                    LocalDate proxVacina = new LocalDate(date1f.getYear(), date1f.getMonthValue()+4, date1f.getDayOfMonth());
+                    if (proxVacina.compareTo(date2f) < 0){
+                        System.out.println("Não pode dar 2 dose");
+                    }else{
+                        System.out.println("Informe a data da Terceira Dose -\n \bInformar em dd-mm-yyyy\b \n");
+                        String date3 = sc.nextLine();
+                        LocalDate date3f = LocalDate.parse(date2,dtf);
+                        LocalDate proxVacina2 = new LocalDate(date2f.getYear(), date2f.getMonthValue()+4, date2f.getDayOfMonth());
+                        if (proxVacina2.compareTo(date3f) < 0){
+                            System.out.println("Não pode dar 3 dose");
+                        }else{
                                     System.out.println("Funcionou Porra");
-                                }
-                            }
-
-                    */
+                        }
+                    }
                     break;
                 case 2:
                     break;
@@ -172,7 +168,6 @@ public class Main{
 /*
 * Problema private acess do localdate
 * dados enfermeira
-*
 * */
 
 
